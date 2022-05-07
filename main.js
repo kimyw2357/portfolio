30d7c1a77acb52383fac36f7c1bbd0f7fe21584c
kimyw2357 <bubuchachakk@gmail.com>
2022-05-08 00:07:30 +0900

    JS:scroll to section

--- main.js
+++ main.js
@@ -2,11 +2,26 @@
 
 // navbar transparent 
 const navbar = document.querySelector('#navbar');
-const navbarHeigh = navbar.getBoundingClientRect().height;
+const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
-  if (window.scrollY > navbarHeigh) {
+  if (window.scrollY > navbarHeight) {
     navbar.classList.add('navbar--dark');    
   } else {
     navbar.classList.remove('navbar--dark');
   }
 })
+
+// Handle scrolling navbr menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+  const link = event.target.dataset.link;
+  if (!link) return;
+  const target = document.querySelector(link);
+  const targetY = target.getBoundingClientRect().y;
+  const distance = window.pageYOffset + targetY - navbarHeight;
+  window.scroll({
+    left: 0,
+    top: distance,
+    behavior: 'smooth',
+  });
+});
