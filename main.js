8eb153e93fa57e5c87d082ed4063a37681403b58
kimyw2357 <bubuchachakk@gmail.com>
2022-05-08 11:16:26 +0900

    JS: home opacity

--- main.js
+++ main.js
@@ -25,6 +25,14 @@ homeContactBtn.addEventListener('click', () => {
   scrollIntoView('#contact');
 });
 
+// home opacity 
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+  home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
+
 function scrollIntoView(selector) {
   const target = document.querySelector(selector);
   const targetY = target.getBoundingClientRect().y;
